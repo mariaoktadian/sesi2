fb59dffe381fb7fd69742e186c27cdcc9f01ba1d
mariaoktadian <127808938+mariaoktadian@users.noreply.github.com>
2023-03-18 11:16:44 +0700

    Add files via upload

---
+++ sesi2/latihan1.php
@@ -0,0 +1,7 @@
+<?php
+    echo "Belajar Script PHP";
+    $dta = "mariaa manies";
+    $angka = 1;
+    echo '<br>nama '.$dta,"peringkat ke-".$angka ;
+    echo '<br>nama $dta'. "anak ke-,". $angka; 
+    echo "<br>nama: $dta anak ke-$angka";
---
+++ sesi2/latihan3.php
@@ -0,0 +1,8 @@
+<?php
+    $kondisiAwal = 3;
+
+    if($kondisiAwal == 1){
+        echo "Kondisi Awal sama dengan 1";
+    }else{
+        echo "Kondisi Awal tidak sama dengan 1";
+    }
